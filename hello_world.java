59dcf4bd545ac24dd1d906c8999ee0cc61a22f92
tarunJ <tarun@gmail.com>
2022-07-12 18:08:57 +0000

    modified

--- hello_world.java
+++ hello_world.java
@@ -1,9 +1,5 @@
-package HelloWorld;
-
-public class Test
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Test");
-    }
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello,World");
+	}
 }
